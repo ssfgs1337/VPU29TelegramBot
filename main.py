8a8867727236e238160a9661d34b2d54ed5bd219
Vasyl Sovyak <soulshockers@gmail.com>
2024-04-11 08:49:46 +0300

    Implemented contact handler

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 from dotenv import load_dotenv
@@ -19,6 +19,7 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [KeyboardButton('Share my location', request_location=True)],
+        [KeyboardButton('Share my contact', request_contact=True)],
     ]
 
     reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
@@ -41,6 +42,20 @@ async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'lat = {lat}, lon = {lon}')
 
 
+async def contact(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.contact.user_id
+    first_name = update.message.contact.first_name
+    last_name = update.message.contact.last_name
+
+    await update.message.reply_text(
+        f"""
+        user_id = {user_id}
+        first_name = {first_name}
+        last_name = {last_name}
+        """,
+        reply_markup=ReplyKeyboardRemove()
+    )
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
@@ -52,5 +67,8 @@ if __name__ == '__main__':
 
     location_handler = MessageHandler(filters.LOCATION, location)
     application.add_handler(location_handler)
+
+    contact_handler = MessageHandler(filters.CONTACT, contact)
+    application.add_handler(contact_handler)
 
     application.run_polling()
